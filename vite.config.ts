5d87f9dbeaee66d63a1af79d46b1512a59e91996
alisher1119 <ali.xay@mail.ru>
2025-05-28 12:09:06 +0500

    add .

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import {defineConfig} from "vite"
-// import dts from 'vite-plugin-dts';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), dts()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
